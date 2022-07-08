dbdebf90233af3094abe171ca89776b9cb751160
jakeyvee <jakeyveee@gmail.com>
2022-07-08 20:08:17 +0800

    ugly ui but works

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createDefaultAuthorizationResultCache, SolanaMobileWalletAdapter } from '@solana-mobile/wallet-adapter-mobile';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { ConnectionProvider, useAnchorWallet, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import {
     GlowWalletAdapter,
@@ -9,8 +9,10 @@ import {
     SolflareWalletAdapter,
     TorusWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo } from 'react';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import React, { FC, ReactNode, useEffect, useMemo, useState } from 'react';
+import { LastTenClient } from './client';
+import * as ltIdl from './client/last_to_ten.json';
 
 export const App: FC = () => {
     return (
@@ -55,5 +57,39 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 const Content: FC = () => {
-    return <WalletMultiButton />;
+    const wallet = useAnchorWallet();
+    const [bucketSize, setBucketSize] = useState(0);
+    const conn = new Connection('https://api.devnet.solana.com');
+    const program = new PublicKey('6DXeNKvUyeqeqdbCwP9VTTedJd3e2MCiuf1KWZBdhoJY');
+
+    const refreshSize = async () => {
+        const ltClient = new LastTenClient(conn, wallet as any, ltIdl as any, program);
+        const bucket = await ltClient.fetchBucketAcc();
+        setBucketSize(bucket.volume);
+    }
+
+    const onTrigger = async () => {
+        if (wallet) {
+            const ltClient = new LastTenClient(conn, wallet as any, ltIdl as any, program);
+            const [pda, _] = await ltClient.findBucketPDA();
+            ltClient.fillBucket(pda, wallet?.publicKey);
+        }
+    }
+
+    useEffect(() => {
+        (async () => {
+            if (wallet) {
+                await refreshSize();
+            }
+        })();
+    }, [wallet]);
+
+    return (
+        <div className='flex flex-col'>
+            <h1> Bucket Size: {bucketSize} </h1>
+            <a className='bg-blue-900 text-center cursor-pointer rounded p-4' onClick={onTrigger}> Fill Bucket </a>
+            <br></br>
+            <WalletMultiButton />
+        </div>
+    );
 };
